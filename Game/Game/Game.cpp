7260c1b2ac83d7b887166a339d2522efb6168290
Oskar Mendel <brokenprogrammer@gmail.com>
2018-10-31 18:15:34 +0100

    Switched to using SDL_Renderer and added basic input handling outline.

--- Game/Game/Game.cpp
+++ Game/Game/Game.cpp
@@ -1,13 +1,11 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+bool isRunning = true;
 
 int main(int argc, char* args[])
 {
@@ -17,34 +15,74 @@ int main(int argc, char* args[])
 	//The surface contained by the window
 	SDL_Surface* screenSurface = NULL;
 
+
 	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return 1;
 	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
 
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+	SDL_ShowCursor(SDL_DISABLE);
 
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Wait two seconds
-			SDL_Delay(2000);
-		}
+	window = SDL_CreateWindow("Game", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+	if (window == NULL) {
+		printf("SDL Couldn't create window! SDL_ERROR: %s\n", SDL_GetError());
+		return 1;
 	}
+
+	SDL_Renderer* const renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == NULL) {
+		printf("SDL Couldn't create renderer! SDL_ERROR: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	if (SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND) < 0) {
+		printf("SDL Couldn't set blend mode! SDL_ERROR: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	//TODO: Support for SDL joysticks
+
+	//TODO: Initialize Sound
+	
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_Rect rect;
+	rect.h = SCREEN_HEIGHT;
+	rect.w = SCREEN_WIDTH;
+	rect.x = 0;
+	rect.y = 0;
+
+	const Uint8 *const keyboardState = SDL_GetKeyboardState(NULL);
+
+	SDL_Event e;
+	while (isRunning) {
+		SDL_RenderFillRect(renderer, &rect);
+		SDL_RenderPresent(renderer);
+
+		if (keyboardState[SDL_SCANCODE_A]) {
+			SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		}
+
+		if (SDL_PollEvent(&e)) {
+			switch (e.type) {
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			case SDL_KEYDOWN:
+				switch (e.key.keysym.sym) {
+				case SDLK_k:
+					SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+					break;
+				}
+				break;
+			}
+		}
+		
+	}
+
+	//Destroy Renderer
+	SDL_DestroyRenderer(renderer);
 
 	//Destroy window
 	SDL_DestroyWindow(window);
